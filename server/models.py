1d3eb08691bd93bed75d3b2ad2ea9a66908b0bd0
roselyne30 <roselyne.lutomia@student.moringaschool.com>
2025-05-19 10:46:37 +0300

    serailizer

--- server/models.py
+++ server/models.py
@@ -2,7 +2,7 @@
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy_serializer import SerializerMixin  # ✅ Make sure this is imported
 
 convention = {
     "ix": "ix_%(column_0_label)s",
@@ -13,12 +13,13 @@ convention = {
 }
 
 metadata = MetaData(naming_convention=convention)
-
 db = SQLAlchemy(metadata=metadata)
 
+# ✅ Add SerializerMixin and serialize_rules to each model
 
-class Zookeeper(db.Model):
+class Zookeeper(db.Model, SerializerMixin):
     __tablename__ = 'zookeepers'
+    serialize_rules = ('-animals.zookeeper',)  # prevent recursion
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
@@ -27,8 +28,9 @@ class Zookeeper(db.Model):
     animals = db.relationship('Animal', back_populates='zookeeper')
 
 
-class Enclosure(db.Model):
+class Enclosure(db.Model, SerializerMixin):
     __tablename__ = 'enclosures'
+    serialize_rules = ('-animals.enclosure',)  # prevent recursion
 
     id = db.Column(db.Integer, primary_key=True)
     environment = db.Column(db.String)
@@ -37,8 +39,9 @@ class Enclosure(db.Model):
     animals = db.relationship('Animal', back_populates='enclosure')
 
 
-class Animal(db.Model):
+class Animal(db.Model, SerializerMixin):
     __tablename__ = 'animals'
+    serialize_rules = ('-zookeeper.animals', '-enclosure.animals',)  # prevent recursion
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
